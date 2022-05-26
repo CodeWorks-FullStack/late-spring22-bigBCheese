cfdba9c76abbcd80f8c3e1ab3bae4d4f3841d5e5
Mick Shannahan <MickShanny@gmail.com>
2022-05-26 15:13:35 -0600

    more notes

--- app/Utils/LocalStorage.js
+++ app/Utils/LocalStorage.js
@@ -12,14 +12,17 @@ export function saveState(){
     parties : ProxyState.parties,
     items: ProxyState.items
   }
+  // NOTE bigBCheese is name this data will be stored under in local storage.
   window.localStorage.setItem('bigBCheese', JSON.stringify(data))
 }
 
 export function loadState(){
   console.log('loading');
+  // NOTE collect the data from where we saved it in save and check if there was any.
   let data = window.localStorage.getItem('bigBCheese')
   if(data){
     let obj = JSON.parse(data)
+    // NOTE when data is saved into local storage it looses it's class, so it much be turned back into the class it was before saving
     ProxyState.parties = obj.parties.map(pd => new Party(pd))
     ProxyState.items = obj.items.map(itd => new Item(itd))
   }
